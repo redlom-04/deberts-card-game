10001df6aa80b84b6f0cfdb72c17e0b1f4741be3
Alex Mostovy <redlom04@gmail.com>
2021-07-06 19:28:28 +0300

    add read file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,28 @@
+import java.io.FileNotFoundException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 	public static void main(String[] args) {
+		try {
+			String result = readFilePath("src/main/resources/text.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
 
+		}
+	}
+
+	private static String readFilePath(String path) throws IOException {
+		FileReader fileReader = new FileReader(path);
+		StringBuilder stringBuilder = new StringBuilder();
+		int value = 0;
+		while (value != -1) {
+			value = fileReader.read();
+			if (value != -1) {
+				stringBuilder.append((char) value);
+			}
+		}
+		return stringBuilder.toString();
 	}
 }
