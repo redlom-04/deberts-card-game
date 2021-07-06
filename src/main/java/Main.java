f3120085a84c0b3730eb49a1c731eae1c3b26c17
Alex Mostovy <redlom04@gmail.com>
2021-07-06 19:47:04 +0300

    added the writeTextFromFileByPath

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,35 @@
 import java.io.FileNotFoundException;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
 	public static void main(String[] args) {
+
 		try {
-			String result = readFilePath("src/main/resources/text.txt");
+			writeTextFromFileByPath("asdf", "src/main/resources/text.txt");
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
 
+		try {
+			String result = readTextFromFileByPath("src/main/resources/text.txt");
+			System.out.println(result);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("File didn't found");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Sorry we can't read the file");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Sorry we didn't know the error");
 		}
 	}
 
-	private static String readFilePath(String path) throws IOException {
+	private static String readTextFromFileByPath(String path) throws IOException {
 		FileReader fileReader = new FileReader(path);
 		StringBuilder stringBuilder = new StringBuilder();
 		int value = 0;
@@ -23,6 +39,14 @@ public class Main {
 				stringBuilder.append((char) value);
 			}
 		}
+		fileReader.close();
 		return stringBuilder.toString();
 	}
+	private static void writeTextFromFileByPath(String text, String path) throws IOException {
+		FileWriter fileWriter = new FileWriter(path);
+		fileWriter.write(text);
+		fileWriter.flush();
+		fileWriter.close();
+	}
+
 }
